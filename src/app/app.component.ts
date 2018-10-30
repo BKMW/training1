567d325723a8c0a115548ac0ade17e60a6a9afb4
Fawzi Benamor <f.benamor@root.sa>
2018-10-30 16:51:12 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  post: {
+    title: string,
+    content: string,
+    loveIts: number,
+    created_at: Date
+  };
+  posts=[
+    {
+      title: "Mon premier post",
+      content: "The ng new command prompts you for information about features to include in the initial app project. Accept the defaults by pressing the Enter or Return key.",
+      loveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: "Mon deuxième post",
+      content: "The ng new command prompts you for information about features to include in the initial app project. Accept the defaults by pressing the Enter or Return key.",
+      loveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: "Encore un post",
+      content: "The ng new command prompts you for information about features to include in the initial app project. Accept the defaults by pressing the Enter or Return key.",
+      loveIts: 0,
+      created_at: new Date()
+    },
+  ];
+  /**
+   *
+   */
+  constructor() {
+  }
 }
